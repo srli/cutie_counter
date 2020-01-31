d28f818806dad841fccb5141bb66b2b272531142
srli <li.sophie.43@gmail.com>
2020-01-31 17:02:57 -0500

    Add support for backgrounds

---
+++ assets/holmes/README.md
@@ -0,0 +1,2 @@
+This is a sequence of placeholder images to demonstrate expression sheets.  
+It will not be part of the final product.
--- src/cutie.ts
+++ src/cutie.ts
@@ -1,25 +1,51 @@
+export enum CutieName {
+    RYDER = 'ryder',
+    HOLMES = 'holmes',
+}
+
+export enum CutieExpression {
+    NEUTRAL = 'neutral',
+    HAPPY = 'happy',
+    CURIOUS = 'curious'
+}
+
+export enum BackgroundState {
+    ROOM_DAYTIME = 'room_daytime'
+}
+
 /**
  * This master class encompasses all the cutie related actions
  */
 class CutiePane {
     private container: HTMLElement;
-    constructor(readonly containerId: string) {
+
+    private readonly cutieBgId: string = 'cutieBg';
+    private readonly cutieImgId: string = 'cutieImage';
+
+    constructor(readonly containerId: string, readonly cutieName: CutieName) {
         this.container = document.querySelector(containerId);
+        this.initializePane();
     };
 
-    public displayImage() {
+    public initializePane() {
         // TODO: Figure out how to overlay background and actual image
         const bg: HTMLImageElement = new Image();
-        bg.src = 'assets/mugenpaintover.png';
-        bg.style.cssText = 'height: 100%; width: 100%; object-fit: contain; z-index: 1';
+        bg.id = this.cutieBgId;
+        bg.src = `assets/backgrounds/${BackgroundState.ROOM_DAYTIME}.png`;
+        bg.style.cssText = 'position: absolute; height: 100%; width: 100%; object-fit: cover; z-index: 1';
         this.container.appendChild(bg);
 
         const cutieImage: HTMLImageElement = new Image();
-        cutieImage.src = 'assets/ryder_normal.png';
-        cutieImage.style.cssText = 'height: 100%; width: 100%; object-fit: contain; z-index:5';
+        cutieImage.id = this.cutieImgId;
+        cutieImage.src = `assets/${this.cutieName}/neutral.png`;
+        cutieImage.style.cssText = 'position: absolute; margin-top: 50px; margin-bottom: 0; height: 100%; width: 100%; object-fit: cover; z-index:5';
         this.container.appendChild(cutieImage);
+    }
+
+    public changeExpression(expression: CutieExpression) {
+        const img:HTMLImageElement = document.querySelector(`#${this.cutieImgId}`);
+        img.src = `assets/${this.cutieName}/${expression}.png`;
     }
 }
 
-const ryderPane: CutiePane = new CutiePane("#cutie");
-ryderPane.displayImage();
+const pane: CutiePane = new CutiePane("#cutie", CutieName.HOLMES);
